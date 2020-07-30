e3acfb5d924e52477c148ecc9c5aa6e667201304
Amy Karnaze <akarnaze@gmail.com>
2020-07-29 20:57:02 -0600

    Add userData fetch

---
+++ src/api.js
@@ -0,0 +1,9 @@
+ function userData() {
+   let usersData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+     .then(response => response.json())
+     .then(data => {
+       return data.users;
+     })
+     .catch(err => console.log(err.message));
+}
+
