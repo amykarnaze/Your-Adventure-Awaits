49a683dca8efccf984c2628161217eec5cd099ae
Amy Karnaze <akarnaze@gmail.com>
2020-07-30 15:53:49 -0600

    Add name property test and class property, allow for different ids and names

--- test/user-test.js
+++ test/user-test.js
@@ -3,8 +3,9 @@ import chai from 'chai';
 import User from '../src/User.js';
 
 describe('User', () => {
-  let user;
   let users;
+  let user;
+  let user2;
   beforeEach(function () {
     users = [
       {
@@ -20,7 +21,8 @@ describe('User', () => {
         "name": "Kelvin Schiller"
       }
     ];
-    user = new User(users[1]);
+    user = new User(users[0]);
+    user2 = new User(users[1]);
 }) 
 
   it('should be a function', function () {
@@ -32,6 +34,19 @@ describe('User', () => {
   });
 
   it('should have a unique id', function () {
-    expect(user.id).to.equal();
+    expect(user.id).to.equal(1);
   });
+
+  it('should have a different unique id', function () {
+    expect(user2.id).to.equal(2);
+  });
+
+  it('should have a name', function () {
+    expect(user.name).to.equal('Leatha Ullrich');
+  });
+
+  it('should have a different name', function () {
+    expect(user2.name).to.equal("Rocio Schuster");
+  });
+
 })
