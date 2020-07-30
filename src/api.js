eb827e9e67925c187b238108239eda7117f826b1
Amy Karnaze <akarnaze@gmail.com>
2020-07-29 20:57:22 -0600

    Add fetched room data

--- src/api.js
+++ src/api.js
@@ -7,3 +7,12 @@
      .catch(err => console.log(err.message));
 }
 
+function roomData() {
+  let roomsData = fetch('https: //fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+    .then(response => response.json())
+    .then(data => {
+      return data.rooms;
+    })
+    .catch(err => console.log(err.message))
+}
+
